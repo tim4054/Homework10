ff6e83eb0c912b7e1783d1af61450cf7a7f03877
tim40 <tim4054@gmail.com>
2024-05-06 20:49:05 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("task1");
+        String firstName = "Ivan";
+        firstName = firstName + " ";
+        String middleName = "Ivanovich";
+        middleName = middleName + " ";
+        String lastName = "Ivanov";
+        lastName = lastName + " ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        System.out.println("task2");
+        System.out.println(fullName.toUpperCase());
+
+        System.out.println("task3");
+        fullName = "Иванов Семён Семёнович";
+        String fullNameTrue = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameTrue);
+
+
     }
 }
